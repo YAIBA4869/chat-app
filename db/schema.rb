c6ccb495b0541a07064ad26d0a425e994173f38f
YAIBA4869 <daisuke.0104yuki@gmail.com>
2026-01-13 14:52:54 +0900

    roomsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_11_081721) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_13_055035) do
+  create_table "rooms", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
